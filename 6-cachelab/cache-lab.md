dc7212e5c322ad32ba423738d581ed122c758302
Run-JB506 <vvzz_run@outlook.com>
2019-02-22 17:42:26 +0800

    complete Cachelab

--- 6-cachelab/cache-lab.md
+++ 6-cachelab/cache-lab.md
@@ -11,20 +11,27 @@
 1. 此处的Cache模型为：
 
     一个set_line的地址结构为：
+
     | tags(-t) | sets(-s) | block(-b)|
     |-----------|--------|------|
+
     set_line的内容结构为：
+
     | last_used | valid | tag | block |
     | --------- | ----- | --- | ----- |
+
     其中last_used标志，上一次是多久使用的，其值越小代表越久未使用
     一个set为：
+
     |set|
     |----|
     |line_0|
     |line_1|
     |...|
     |line_E-1|
+
     而一个Cache模型为：
+
     |Cache|
     |-----|
     |set_0|
@@ -37,7 +44,23 @@
 4. 执行LRU(最近未使用)算法进行页替换
     - 首先Cache初始为空，那么此时要将read的块存入Cache.对于这种情况，我们首先要找空行，即find_emptyline.
     如果该行中的valid无效，代表为空，可以存入，返回其index。否则返回-1.
-    - 
-    
 
 
+
+### PartB: 使用blocking技术完成矩阵的转置
+根据材料得知,s=5 b=5,共有32个sets,一个sets的块大小为32Byte.即一个块可以放入8个int.
+故如果按照默认的来排列(假设为32*32),则情况如下.
+  ![](https://i.imgur.com/t2WrTbG.png) 
+即按顺序存入cache时,第一行和第九行会发生conflict miss.这样会发生一个问题,转置为```B[j][i]=A[i][j]```.在对矩阵B的时候,这样默认的方式会造成更多的miss.所以要采用blocking技术.
+
+- M == 32 N == 32
+将32*32的矩阵分成8*8的小block,这样会减少miss数目.如下图分析:
+![](https://i.imgur.com/CP54U2s.png)
+其中1-8 9-16....等都会各自存入一个set中,这样在转置到B的时候,B的一行都可以从这8个set中取出,即hit.比默认的miss数目大量减少.
+故我们得到结果如下:
+![](https://i.imgur.com/sUehfDJ.png) 
+我们发现miss数目仍然大于300. 为进一步减小miss,我们发现对于对角线上的数目可以不用```B[j][i]=A[i][j]```.这样也就减少了miss数,对于对角线上的元素,用一个临时变量保存即可.再次实验:  
+![](https://i.imgur.com/qwWT4hb.png)  
+
+- M == 64 N ==64
+我们继续按照8*8的块大小来分,发现次数大于要求的miss.
--- 6-cachelab/cache-lab/cachelab-handout/trans.c
+++ 6-cachelab/cache-lab/cachelab-handout/trans.c
@@ -1,4 +1,4 @@
-/* 
+﻿/*
  * trans.c - Matrix transpose B = A^T
  *
  * Each transpose function must have a prototype of the form:
@@ -6,44 +6,104 @@
  *
  * A transpose function is evaluated by counting the number of misses
  * on a 1KB direct mapped cache with a block size of 32 bytes.
- */ 
+ */
 #include <stdio.h>
 #include "cachelab.h"
 
 int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 
-/* 
+/*
  * transpose_submit - This is the solution transpose function that you
  *     will be graded on for Part B of the assignment. Do not change
  *     the description string "Transpose submission", as the driver
  *     searches for that string to identify the transpose function to
- *     be graded. 
+ *     be graded.
  */
 char transpose_submit_desc[] = "Transpose submission";
-void transpose_submit(int M, int N, int A[N][M], int B[M][N])
-{
+void transpose_submit(int M, int N, int A[N][M], int B[M][N]) {
+  /*
+   */
+  int col;
+  int row;
+  int c;
+  int r;
+  int buffer[8];  // buffer[0]=temp,buffer[1]=d
+  if (N == 32) {
+    // block size = 8 * 8
+    for (row = 0; row < N; row += 8) {
+      for (col = 0; col < M; col += 8) {
+        for (r = row; r < row + 8; r++) {
+          for (c = col; c < col + 8; c++) {
+            if (r != c)
+              B[c][r] = A[r][c];
+            else {
+              buffer[0] = A[r][c];
+              buffer[1] = r;
+            }
+          }
+          if (row == col) B[buffer[1]][buffer[1]] = buffer[0];
+        }
+      }
+    }
+  } else if (N == 64) {
+    // block size is 8 * 8;
+    for (row = 0; row < N; row += 8) {
+      for (col = 0; col < M; col += 8) {
+        for (r = row; r < row + 4; r++) {
+          for (c = 0; c < 8; c++) buffer[c] = A[r][col + c];
+          for (c = 0; c < 4; c++) {
+            B[col + c][r] = buffer[c];
+            B[col + c][r + 4] = buffer[c + 4];
+          }
+        }
+
+        for (c = col; c < col + 4; c++) {
+          for (r = 0; r < 4; r++) buffer[r] = B[c][row + 4 + r];
+          for (r = 4; r < 8; r++) B[c][row + r] = A[row + r][c];
+          for (r = 0; r < 4; r++) B[c + 4][row + r] = buffer[r];
+          for (r = 4; r < 8; r++) B[c + 4][row + r] = A[row + r][c + 4];
+        }
+      }
+    }
+  } else {
+    // M==61 N==67 block size is 8*8.
+    // is the same as N==32
+    for (row = 0; row < 67; row += 16) {
+      for (col = 0; col < 61; col += 16) {
+        for (r = row; r < row + 16 && r < 67; r++) {
+          for (c = col; c < col + 16 && c < 61; c++) {
+            if (r != c)
+              B[c][r] = A[r][c];
+            else {
+              buffer[0] = A[r][c];
+              buffer[1] = r;
+            }
+          }
+          if (row == col) B[buffer[1]][buffer[1]] = buffer[0];
+        }
+      }
+    }
+  }
 }
 
-/* 
+/*
  * You can define additional transpose functions below. We've defined
- * a simple one below to help you get started. 
- */ 
+ * a simple one below to help you get started.
+ */
 
-/* 
+/*
  * trans - A simple baseline transpose function, not optimized for the cache.
  */
 char trans_desc[] = "Simple row-wise scan transpose";
-void trans(int M, int N, int A[N][M], int B[M][N])
-{
-    int i, j, tmp;
+void trans(int M, int N, int A[N][M], int B[M][N]) {
+  int i, j, tmp;
 
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < M; j++) {
-            tmp = A[i][j];
-            B[j][i] = tmp;
-        }
-    }    
-
+  for (i = 0; i < N; i++) {
+    for (j = 0; j < M; j++) {
+      tmp = A[i][j];
+      B[j][i] = tmp;
+    }
+  }
 }
 
 /*
@@ -53,32 +113,28 @@ void trans(int M, int N, int A[N][M], int B[M][N])
  *     performance. This is a handy way to experiment with different
  *     transpose strategies.
  */
-void registerFunctions()
-{
-    /* Register your solution function */
-    registerTransFunction(transpose_submit, transpose_submit_desc); 
+void registerFunctions() {
+  /* Register your solution function */
+  registerTransFunction(transpose_submit, transpose_submit_desc);
 
-    /* Register any additional transpose functions */
-    registerTransFunction(trans, trans_desc); 
-
+  /* Register any additional transpose functions */
+  registerTransFunction(trans, trans_desc);
 }
 
-/* 
+/*
  * is_transpose - This helper function checks if B is the transpose of
  *     A. You can check the correctness of your transpose by calling
  *     it before returning from the transpose function.
  */
-int is_transpose(int M, int N, int A[N][M], int B[M][N])
-{
-    int i, j;
+int is_transpose(int M, int N, int A[N][M], int B[M][N]) {
+  int i, j;
 
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < M; ++j) {
-            if (A[i][j] != B[j][i]) {
-                return 0;
-            }
-        }
+  for (i = 0; i < N; i++) {
+    for (j = 0; j < M; ++j) {
+      if (A[i][j] != B[j][i]) {
+        return 0;
+      }
     }
-    return 1;
+  }
+  return 1;
 }
-
